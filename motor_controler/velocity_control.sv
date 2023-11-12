a8dc0e85f748eca8789dc13d88dc009169e2de04
Juan <68251336+El-Guapo2024@users.noreply.github.com>
2023-11-11 19:50:04 -0500

    pwm

---
+++ motor_controler/velocity_control.sv
@@ -0,0 +1,10 @@
+module  pwm(
+            input wire clk_in,
+            input wire rst_in,
+            input wire signed [7:0] level_in,
+            input wire direction,
+            output logic pwm_out
+  );
+ 
+endmodule
+`default_nettype wire
